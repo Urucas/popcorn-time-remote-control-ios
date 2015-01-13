88ff9c6ce4daa6905efd2c8ad4d6c80eb8060122
vrunoa <vruno.alassia@gmail.com>
2015-01-13 03:15:48 -0300

    add ux

--- PopcornTimeRemoteControl/Controllers/LeftMenuViewController.h
+++ PopcornTimeRemoteControl/Controllers/LeftMenuViewController.h
@@ -11,7 +11,6 @@
 
 @interface LeftMenuViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
-@property (nonatomic, strong) IBOutlet UITableView *tableView;
 @property (nonatomic, assign) BOOL slideOutAnimationEnabled;
 
 @end
--- PopcornTimeRemoteControl/Controllers/LeftMenuViewController.m
+++ PopcornTimeRemoteControl/Controllers/LeftMenuViewController.m
@@ -28,63 +28,7 @@
 {
 	[super viewDidLoad];
 	
-	self.tableView.separatorColor = [UIColor lightGrayColor];
 	
-}
-
-#pragma mark - UITableView Delegate & Datasrouce -
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-	return 4;
-}
-
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
-{
-	UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.frame.size.width, 20)];
-	view.backgroundColor = [UIColor clearColor];
-	return view;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
-{
-	return 20;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"leftMenuCell"];
-	
-	switch (indexPath.row) {
-		case 0:
-			cell.textLabel.text = @"Home";
-			break;
-			
-    }
-	
-	cell.backgroundColor = [UIColor clearColor];
-	
-	return cell;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-	UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"MainStoryboard_iPhone"
-															 bundle: nil];
-	UIViewController *vc ;
-	
-	switch (indexPath.row)
-	{
-		case 0:
-			vc = [mainStoryboard instantiateViewControllerWithIdentifier: @"HomeViewController"];
-			break;
-		
-	}
-	
-	[[SlideNavigationController sharedInstance] popToRootAndSwitchToViewController:vc
-        withSlideOutAnimation:self.slideOutAnimationEnabled
-        andCompletion:nil
-     ];
 }
 
 @end
